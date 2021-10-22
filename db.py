9ab84ff1730b0195bb88a72aced081c5667e89c0
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-10-22 22:43:15 +0800

    sqlalchemy

---
+++ db.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(120), nullable=False)
+    rating = db.Column(db.Float(10), nullable=False)
+
--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-from flask import Flask, render_template, request
+from db import db, Books, app, request, render_template
 
-
-app = Flask(__name__)
-
+# db.create_all()
 all_books = []
+
+new_entry = Books(title="f Potter", author="J.K Rowlling", rating="9.5")
+db.session.add(new_entry)
+db.session.commit()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", books=all_books)
 
 
-@app.route('/add')
+@app.route('/add', methods=['POST', 'GET'])
 def add_books():
+    if request.method == "POST":
+        all_books.append({
+            "title": request.form['book_name'],
+            "author": request.form['book_author'],
+            "rating": request.form['book_rating']
+        })
 
     return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+'''
+IF WER WERE USING SQLITE3
+
+# Initialize connection
+db = sqlite3.connect("books-collection.db")
+
+cursor = db.cursor()
+
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) "
+#                "NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+
+cursor.execute("INSERT INTO books VALUES(2, 'Not Mint', ' Rowling', '5')")
+db.commit()
+
+'''
